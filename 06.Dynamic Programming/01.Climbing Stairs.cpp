e0f556ad7027044063ee6e879e469d9aef614747
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2025-05-07 14:31:04 +0530

    Create 01.Climbing Stairs.cpp

---
+++ 06.Dynamic Programming/01.Climbing Stairs.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 0 || n == 1) return 1;
+        int a=1,b=1;
+        for(int i=2;i<=n;i++){
+            int temp = a+b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }     
+};
+/*
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 0 || n == 1) return 1;
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-2]+dp[i-1];
+        }
+        return dp[n];
+    }     
+};
+
+*/
