5e7330d8d87476a1eedb252f68ff7a97e957cd5b
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2025-05-09 07:31:17 +0530

    Create 04.Pascal's Triangle.cpp

---
+++ 09.Others/04.Pascal's Triangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
+        for(int row=0;row<numRows;row++){
+            vector<int>currentRow(row+1,1);
+
+            for(int j=0;j<row;j++){
+                currentRow[j] = triangle[row-1][j-1] + triangle[row-1][j];
+            }
+            triangle.push_back(currentRow);
+        }
+        return triangle;
+    }
+};
